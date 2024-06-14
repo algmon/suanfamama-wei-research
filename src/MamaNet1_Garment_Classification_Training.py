dcf70f68f2f56b5d372c26fda35423e21352e9ec
Wei Jiang <wei@suanfamama.com>
2024-06-14 17:26:22 +0800

    ADD templete for MamaNet1

---
+++ src/MamaNet1_Garment_Classification_Training.py
@@ -0,0 +1,12 @@
+# MamaNet1 Garment Classification Training
+"""
+This module is developed by Suanfamama.
+
+Authors:
+- Wei Jiang (wei@suanfamama.com)
+- Mama Xiao (mama.xiao@suanfamama.com)
+
+Reference:
+- Training with PyTorch
+- https://pytorch.org/tutorials/beginner/introyt/trainingyt.html?highlight=nn%20crossentropyloss
+"""
